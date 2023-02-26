05174d0780ce3d01cc2f6e1bc369cd2e33f85174
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 10:18:48 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_3_4
+{
+    class Program
+    {
+        public delegate void SubstractSumDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            SubstractSumDelegate substractSumDelegate = Substract;
+            substractSumDelegate += Sum;
+            substractSumDelegate(10, 5);
+            
+            Console.ReadKey();
+        }
+
+        static void Substract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
